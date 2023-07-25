5181dc8c469f08332090f9d100c3f09a3b8309d3
charlesgit670 <69318601+charlesgit670@users.noreply.github.com>
2023-07-25 16:44:58 +0200

    Update main.py
    set path

--- visualisation/main.py
+++ visualisation/main.py
@@ -2,9 +2,9 @@ from read_data_stream import read_most_recent_csv
 from main_visualisation import monitoring_today_validation
 
 if __name__ == "__main__":
-    directory_total_path = ""
-    directory_titre_path = ""
-    directory_arret_path = ""
+    directory_total_path = "../data/ouput3"
+    directory_titre_path = "../data/output2"
+    directory_arret_path = "../data/output"
 
     df_total = read_most_recent_csv(directory_total_path)
     df_titre = read_most_recent_csv(directory_titre_path)
